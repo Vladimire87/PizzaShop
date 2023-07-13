786c3b0a1b1f8099fe9370b155983dd1e19060f4
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-07-13 19:20:32 +0400

    add js for updating cart items count

--- public/app.js
+++ public/app.js
@@ -6,5 +6,21 @@ function something () {
 }
 
 function add_to_cart (id) {
-  alert('id: ' + id)
+  var key = 'product_' + id
+  var x = window.localStorage.getItem(key)
+  x = x * 1 + 1
+  window.localStorage.setItem(key, x)
 }
+document.addEventListener('DOMContentLoaded', function () {
+  var itemNumber = 0
+  for (const [key, value] of Object.entries(localStorage)) {
+    y = Number(value)
+    itemNumber += y
+  }
+  console.log('Total items: ' + itemNumber)
+
+  var item_count = document.getElementById('item_count')
+  if (item_count) {
+    item_count.textContent = itemNumber
+  }
+})
