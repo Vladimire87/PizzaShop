1a05052a5f69a496fff023a62e1874e6655fa0ad
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-07-14 14:47:53 +0400

    cart numbers function

--- public/app.js
+++ public/app.js
@@ -10,6 +10,7 @@ function add_to_cart (id) {
   var x = window.localStorage.getItem(key)
   x = x * 1 + 1
   window.localStorage.setItem(key, x)
+  console.log('Items in your cart: ' + cart_get_number_of_items())
 }
 
 function cart_get_number_of_items () {
